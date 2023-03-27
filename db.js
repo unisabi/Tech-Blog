b30b87e1fdf81a56a7b0e308d7bb9403113fefeb
Sabina Ceja-Siete <sabinasiete@gmail.com>
2023-03-27 15:44:52 -0700

    configured Heroku's database

--- db.js
+++ db.js
@@ -3,10 +3,13 @@ require("dotenv").config();
 
 const { DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST, DB_PORT } = process.env;
 
-const connection = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: "mysql",
-});
+const connection =
+ process.env.JAWSDB_URL ? 
+  new Sequelize(process.env.JAWSDB_URL) :
+  new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: "mysql",
+  });
 
 module.exports = connection;
